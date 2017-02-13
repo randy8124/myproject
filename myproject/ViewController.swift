e9509079119f9b1ceb2495397d7425c07a0c899b
Anil Reddy <anilreddy@Anils-MacBook-Pro.local>
2017-02-13 12:14:24 -0500

    added just 1 line

--- myproject/ViewController.swift
+++ myproject/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //its a smple project for R
     }
 
     override func didReceiveMemoryWarning() {
